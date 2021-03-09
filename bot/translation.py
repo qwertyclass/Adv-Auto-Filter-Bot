1ec006993503cdb55d41b385be377891b567566d
qwertyclass <73537167+qwertyclass@users.noreply.github.com>
2021-03-09 13:48:00 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,6 +7,8 @@ class Translation(object):
     START_TEXT = """<b>Hai {}!!</b>
 <i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...
 
+Made For @OPM_Cinemas 
+
 Just Sent Any Text I Will Search In All Connected Chat And Reply You With The Message link
 
 You Can Even Connected To 3 Channels At A Time...</i>"""    
@@ -30,11 +32,11 @@ Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
 
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a herf="https://t.me/Qwertyclass">Qwertyclass</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
 
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
+<b>➥ Source Code</b> : <i><a herf="https://github.com/Qwertyclass/Adv-Auto-Filter-Bot">GitHub</a></i>
 """
